2cf9d9b52452e285a2a3eeaf13915f15fb72a427
Anton Markelov <doublic@gmail.com>
2024-05-14 14:17:15 +0300

    rewrite into modular architecture

--- app/app.py
+++ app/app.py
@@ -1,130 +1,18 @@
 from flask import Flask,Response
 from prometheus_client import Gauge,generate_latest
-import boto3
 from datetime import datetime, timedelta
-import time, os
+import os
 import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
-
-def aws_query():
-    
-    now = datetime.now()
-    yesterday = datetime.today() - timedelta(days=1)
-    tomorrow = datetime.today() + timedelta(days=1)
-    two_days_ago = datetime.today() - timedelta(days=2)
-    nxt_mnth = now.replace(day=28) + timedelta(days=4)
-    firstday_of_nxt_mnth = nxt_mnth.strftime("%Y-%m-01")
-    
-    print("Calculating costs...")
-    
-    if os.environ.get('METRIC_TODAY_DAILY_COSTS') is not None:
-
-        r = client.get_cost_and_usage(
-            TimePeriod={
-                'Start': now.strftime("%Y-%m-%d"),
-                'End':  tomorrow.strftime("%Y-%m-%d")
-            },
-            Granularity="DAILY",
-            Metrics=["BlendedCost"]
-        )
-        cost = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
-        print("Updated AWS Daily costs: %s" %(cost))
-        g_cost.set(float(cost))
-
-    if os.environ.get('METRIC_YESTERDAY_DAILY_COSTS') is not None:
-        
-        r = client.get_cost_and_usage(
-            TimePeriod={
-                'Start': yesterday.strftime("%Y-%m-%d"),
-                'End':  now.strftime("%Y-%m-%d")
-            },
-            Granularity="DAILY",
-            Metrics=["BlendedCost"]
-        )
-        cost_yesterday = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
-        print("Yesterday's AWS Daily costs: %s" %(cost_yesterday))
-        g_yesterday.set(float(cost_yesterday))
-        
-        r = client.get_cost_and_usage(
-            TimePeriod={
-                'Start': yesterday.strftime("%Y-%m-%d"),
-                'End':  now.strftime("%Y-%m-%d")
-            },
-            Granularity="DAILY",
-            Metrics=["BlendedCost"],
-            GroupBy=[
-                {
-                    'Type': 'DIMENSION',
-                    'Key': 'SERVICE'
-                }    
-            ]
-        )
-        
-        services = r["ResultsByTime"][0]["Groups"]
-        # services = sorted(services, key = lambda i: i['Metrics']['BlendedCost']['Amount'])
-        g_yesterday_by_service.clear()
-        for service in services:
-            service_name = service.get('Keys')[0]
-            service_cost = service.get('Metrics')['BlendedCost']['Amount']
-            g_yesterday_by_service.labels(service_name).set(float(service_cost))
-
-    if os.environ.get('METRIC_MONTH_TO_DATE_COSTS') is not None:
-        
-        r = client.get_cost_and_usage(
-            TimePeriod={
-                'Start': now.strftime("%Y-%m-01"),
-                'End':  firstday_of_nxt_mnth
-            },
-            Granularity="MONTHLY",
-            Metrics=["UnblendedCost"]
-        )
-        cost_mtd = r["ResultsByTime"][0]["Total"]["UnblendedCost"]["Amount"]
-        print("Updated Month to date Cost: %s" %(cost_mtd))
-        g_mtd.set(float(cost_mtd))
-        
-    if os.environ.get('METRIC_MONTH_FORCASTED_COSTS') is not None:
-        
-        r = client.get_cost_forecast(
-            TimePeriod={
-                'Start': now.strftime("%Y-%m-%d"),
-                'End': firstday_of_nxt_mnth
-            },
-            Metric='UNBLENDED_COST',
-            Granularity='MONTHLY'
-            )
-        cost_month_forcast = r["Total"]["Amount"]
-        print("This Month's forecasted cost: %s" %(cost_month_forcast))
-        g_month_forcast.set(float(cost_month_forcast))
-
-    print("Finished calculating costs")
-    return 0
+from aws import AWS
 
 app = Flask(__name__)
 
 CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
-client = boto3.client('ce')
-
-METRIC_TODAY_DAILY_COSTS=os.environ.get('METRIC_TODAY_DAILY_COSTS')
-METRIC_YESTERDAY_DAILY_COSTS=os.environ.get('METRIC_YESTERDAY_DAILY_COSTS')
-METRIC_MONTH_TO_DATE_COSTS=os.environ.get('METRIC_MONTH_TO_DATE_COSTS')
-METRIC_MONTH_FORCASTED_COSTS=os.environ.get('METRIC_MONTH_FORCASTED_COSTS')
 QUERY_PERIOD=os.getenv('QUERY_PERIOD', "5400")
 
-if METRIC_TODAY_DAILY_COSTS is not None:
-    g_cost = Gauge('aws_today_daily_costs', 'Today daily costs from AWS')
-if METRIC_YESTERDAY_DAILY_COSTS is not None:
-    g_yesterday = Gauge('aws_yesterday_daily_costs', 'Yesterday daily costs from AWS')
-    g_yesterday_by_service = Gauge("aws_yesterday_costs_by_service", 'Yesterday daily costs from AWS by service', ['aws_service'])
-if METRIC_MONTH_TO_DATE_COSTS is not None:
-    g_mtd = Gauge('aws_month_to_date_costs', 'Month to date costs.')
-if METRIC_MONTH_FORCASTED_COSTS is not None:
-    g_month_forcast = Gauge('aws_month_forcasted_costs', 'Monthly forcasted cost.')
 
-print(f"METRIC_TODAY_DAILY_COSTS: {METRIC_TODAY_DAILY_COSTS}")
-print(f"METRIC_YESTERDAY_DAILY_COSTS: {METRIC_YESTERDAY_DAILY_COSTS}")
-print(f"METRIC_MONTH_TO_DATE_COSTS: {METRIC_MONTH_TO_DATE_COSTS}")
-print(f"METRIC_MONTH_FORCASTED_COSTS: {METRIC_MONTH_FORCASTED_COSTS}")
 print(f"QUERY_PERIOD: {QUERY_PERIOD}")
 print(f"AWS_ACCESS_KEY_ID: {os.environ.get('AWS_ACCESS_KEY_ID')}")
 
@@ -133,13 +21,17 @@ scheduler = BackgroundScheduler()
 print("Job starts at: " + str(datetime.now() + timedelta(seconds=10)) )
 
 scheduler.start()
+
+# TODO make this optional
+aws_client = AWS()
 scheduler.add_job(
-    func=aws_query,
+    func=aws_client.fill_metrics,
     trigger=IntervalTrigger(seconds=int(QUERY_PERIOD),start_date=(datetime.now() + timedelta(seconds=10))),
     id='aws_query',
     name='Run AWS Query',
     replace_existing=True
     )
+
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
 
---
+++ app/aws.py
@@ -0,0 +1,152 @@
+import boto3
+from datetime import datetime, timedelta
+from prometheus_client import Gauge
+import os
+
+
+class AWS:
+    """
+    The AWS class provides methods to retrieve and calculate costs from AWS using the AWS Cost Explorer API.
+    It uses the boto3 library to interact with the AWS Cost Explorer API and the prometheus_client library to expose the costs as Prometheus metrics.
+    """
+
+    client = boto3.client('ce')
+    METRIC_TODAY_DAILY_COSTS=os.environ.get('METRIC_TODAY_DAILY_COSTS')
+    METRIC_YESTERDAY_DAILY_COSTS=os.environ.get('METRIC_YESTERDAY_DAILY_COSTS')
+    METRIC_MONTH_TO_DATE_COSTS=os.environ.get('METRIC_MONTH_TO_DATE_COSTS')
+    METRIC_MONTH_FORCASTED_COSTS=os.environ.get('METRIC_MONTH_FORCASTED_COSTS')
+
+    print(f"METRIC_TODAY_DAILY_COSTS: {METRIC_TODAY_DAILY_COSTS}")
+    print(f"METRIC_YESTERDAY_DAILY_COSTS: {METRIC_YESTERDAY_DAILY_COSTS}")
+    print(f"METRIC_MONTH_TO_DATE_COSTS: {METRIC_MONTH_TO_DATE_COSTS}")
+    print(f"METRIC_MONTH_FORCASTED_COSTS: {METRIC_MONTH_FORCASTED_COSTS}")
+
+    def __init__(self):
+        """
+        Initializes an instance of the AWS class.
+        """
+        pass
+
+    def get_today_daily_costs(self):
+        """
+        Retrieves today's daily costs from AWS.
+
+        Returns:
+            float: The total cost for today.
+        """
+        r = self.client.get_cost_and_usage(
+            TimePeriod={
+                'Start': datetime.now().strftime("%Y-%m-%d"),
+                'End':  (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")
+            },
+            Granularity="DAILY",
+            Metrics=["BlendedCost"]
+        )
+        cost = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
+        return float(cost)
+    
+    def get_yesterday_daily_costs(self):
+        """
+        Retrieves yesterday's daily costs from AWS.
+
+        Returns:
+            float: The total cost for yesterday.
+        """
+        r = self.client.get_cost_and_usage(
+            TimePeriod={
+                'Start': (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d"),
+                'End':  datetime.now().strftime("%Y-%m-%d")
+            },
+            Granularity="DAILY",
+            Metrics=["BlendedCost"]
+        )
+        cost = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
+        return float(cost)
+    
+    def get_yesterday_daily_costs_by_service(self):
+        """
+        Retrieves yesterday's daily costs from AWS grouped by service.
+
+        Returns:
+            list: A list of dictionaries containing the service name and its cost.
+        """
+        r = self.client.get_cost_and_usage(
+            TimePeriod={
+                'Start': (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d"),
+                'End':  datetime.now().strftime("%Y-%m-%d")
+            },
+            Granularity="DAILY",
+            Metrics=["BlendedCost"],
+            GroupBy=[
+                {
+                    'Type': 'DIMENSION',
+                    'Key': 'SERVICE'
+                }    
+            ]
+        )
+        service_costs = r["ResultsByTime"][0]["Groups"]
+        service_costs = sorted(service_costs, key = lambda i: i['Metrics']['BlendedCost']['Amount'])
+        return service_costs
+    
+    def get_month_to_date_costs(self):
+        """
+        Retrieves the month-to-date costs from AWS.
+
+        Returns:
+            float: The total cost for the current month.
+        """
+        r = self.client.get_cost_and_usage(
+            TimePeriod={
+                'Start': datetime.now().strftime("%Y-%m-01"),
+                'End':  (datetime.now().replace(day=28) + timedelta(days=4)).strftime("%Y-%m-01")
+            },
+            Granularity="MONTHLY",
+            Metrics=["UnblendedCost"]
+        )
+        cost = r["ResultsByTime"][0]["Total"]["UnblendedCost"]["Amount"]
+        return float(cost)
+    
+    def get_month_forecasted_costs(self):
+        """
+        Retrieves the forecasted costs for the current month from AWS.
+
+        Returns:
+            float: The forecasted cost for the current month.
+        """
+        r = self.client.get_cost_forecast(
+            TimePeriod={
+                'Start': datetime.now().strftime("%Y-%m-%d"),
+                'End':  (datetime.now().replace(day=28) + timedelta(days=4)).strftime("%Y-%m-01")
+            },
+            Metric='UNBLENDED_COST',
+            Granularity='MONTHLY'
+            )
+        cost = r["Total"]["Amount"]
+        return float(cost)
+
+    def fill_metrics(self):
+        """
+        Fills the Prometheus metrics with the AWS costs.
+
+        This method retrieves the costs from AWS using the methods defined above and sets the corresponding Prometheus metrics.
+        """
+        print(f"{datetime.now()} Calculating AWS costs...")
+        if self.METRIC_TODAY_DAILY_COSTS is not None:
+            aws_today_daily_costs = Gauge('aws_today_daily_costs', 'Today daily costs from AWS')
+            aws_today_daily_costs.set(self.get_today_daily_costs())
+        if self.METRIC_YESTERDAY_DAILY_COSTS is not None:
+            aws_yesterday_daily_costs = Gauge('aws_yesterday_daily_costs', 'Yesterday daily costs from AWS')
+            aws_yesterday_daily_costs.set(self.get_yesterday_daily_costs())
+            aws_yesterday_costs_by_service = Gauge("aws_yesterday_costs_by_service", 'Yesterday daily costs from AWS by service', ['aws_service'])
+            aws_yesterday_costs_by_service.clear()
+            for service in self.get_yesterday_daily_costs_by_service():
+                service_name = service.get('Keys')[0]
+                service_cost = service.get('Metrics')['BlendedCost']['Amount']
+                aws_yesterday_costs_by_service.labels(service_name).set(float(service_cost))
+        if self.METRIC_MONTH_TO_DATE_COSTS is not None:
+            aws_month_to_date_costs = Gauge('aws_month_to_date_costs', 'Month to date costs.')
+            aws_month_to_date_costs.set(self.get_month_to_date_costs())
+        if self.METRIC_MONTH_FORCASTED_COSTS is not None:
+            aws_month_forecasted_costs = Gauge('aws_month_forecasted_costs', 'Monthly forecasted cost.')
+            aws_month_forecasted_costs.set(self.get_month_forecasted_costs())
+        print(f"{datetime.now()} Finished calculating costs...")
